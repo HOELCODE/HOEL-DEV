38060bb93d11b92746d6230429bb9920aa91563a
HOELCODE <hcostantini1@gmail.com>
2025-05-27 22:47:34 +0200

    Début Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import '../css/Header.css';
+
 const Header = () => {
     return (
         <header>
-            <h1>My Application</h1>
+            <p><Link to="/">UN JOLI LOGO</Link></p>
             <nav>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/Parcours">Parcours</Link></li>
+                    <li><Link to="/Projets">Projets</Link></li>
+                    <li><Link to="/Curriculum">Curriculum</Link></li>
+                    <li><Link to="/Contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
