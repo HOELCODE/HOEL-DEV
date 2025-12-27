27ea9e725f17b3e3f0ac36d2fad38359803e1d36
HOELCODE <hcostantini1@gmail.com>
2025-12-27 11:49:23 +0100

    test commit

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,7 +1,7 @@
 const Accueil = () => {
     return (
         <div className="accueil">
-            <h1>Bienvenue sur mon portfolio</h1>
+            <h1>Bienvenue sur mon portfolio test</h1>
             <p>Je suis un développeur passionné par la création d'applications web.</p>
             <p>Explorez mes projets, mon parcours et n'hésitez pas à me contacter !</p>
         </div>
