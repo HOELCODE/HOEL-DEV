cc2378a148949b90bd2967dc029edef5970c5fc5
HOELCODE <hcostantini1@gmail.com>
2026-04-16 11:15:03 +0200

    Calendrier, navigation mois/ année, jours associés, jour selected

--- frontend/src/pages/Calendrier.jsx
+++ frontend/src/pages/Calendrier.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+
 const Calendrier = () => {
+
   // Tableau des mois et jours
   const monthArray = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre'];
   const daysArray = ['LUN', 'MAR', 'MER', 'JEU', 'VEN', 'SAM', 'DIM'];
@@ -15,6 +17,7 @@ const Calendrier = () => {
   // State pour l'année 
   const [year, setYear] = useState(currentYear);
   
+  // Bouton prochain mois
   const nextMonth = () => {
     const currentIndex = monthArray.indexOf(month);
     const nextIndex = (currentIndex + 1) % monthArray.length;
@@ -22,6 +25,7 @@ const Calendrier = () => {
     setMonth(monthArray[nextIndex]);
   }
 
+  // Bouton mois précédent
   const lastMonth = () => {
     const reverseMonthArray = monthArray.reverse();
     const currentIndex = monthArray.indexOf(month);
@@ -30,6 +34,14 @@ const Calendrier = () => {
     setMonth(reverseMonthArray[lastIndex]);
   }
 
+  // Déterminer le chiffre de la date d'ajourd'hui
+  const currentDay = date.getDate();
+  // Déterminer le jour correspondant au premier jour du mois
+  const firstDayOfMonth = new Date(year, monthArray.indexOf(month), 1).getDay();
+  // Déterminer le nombre de jours dans le mois
+  const daysInMonth = new Date(year, monthArray.indexOf(month) + 1, 0).getDate();
+  //Déterminer le chiffre de la date d'ajourd'hui de ce mois et de cette année
+  const currentDayOfMonth = (month === currentMonth && year === currentYear) ? currentDay : null;
 
 
   return (
@@ -51,37 +63,14 @@ const Calendrier = () => {
               ))}
             </div>
             <div className="day">
-              <div className="day-number"><span className="number">1</span></div>
-              <div className="day-number selected"><span className="number">2</span></div>
-              <div className="day-number"><span className="number">3</span></div>
-              <div className="day-number"><span className="number">4</span></div>
-              <div className="day-number"><span className="number">5</span></div>
-              <div className="day-number"><span className="number">6</span></div>
-              <div className="day-number"><span className="number">7</span></div>
-              <div className="day-number"><span className="number">8</span></div>
-              <div className="day-number"><span className="number">9</span></div>
-              <div className="day-number"><span className="number">10</span></div>
-              <div className="day-number"><span className="number">11</span></div>
-              <div className="day-number"><span className="number">12</span></div>
-              <div className="day-number"><span className="number">13</span></div>
-              <div className="day-number"><span className="number">14</span></div>
-              <div className="day-number"><span className="number">15</span></div>
-              <div className="day-number"><span className="number">16</span></div>
-              <div className="day-number"><span className="number">17</span></div>
-              <div className="day-number"><span className="number">18</span></div>
-              <div className="day-number"><span className="number">19</span></div>
-              <div className="day-number"><span className="number">20</span></div>
-              <div className="day-number"><span className="number">21</span></div>
-              <div className="day-number"><span className="number">22</span></div>
-              <div className="day-number"><span className="number">23</span></div>
-              <div className="day-number"><span className="number">24</span></div>
-              <div className="day-number"><span className="number">25</span></div>
-              <div className="day-number"><span className="number">26</span></div>
-              <div className="day-number"><span className="number">27</span></div>
-              <div className="day-number"><span className="number">28</span></div>
-              <div className="day-number"><span className="number">29</span></div>
-              <div className="day-number"><span className="number">30</span></div>
-              <div className="day-number"><span className="number">31</span></div>
+              {Array.from({ length: firstDayOfMonth -1 }, (_, index) => (
+                <div key={index} className="day-number empty"></div>
+              ))}
+              {Array.from({ length: daysInMonth }, (_, index) => (
+                <div key={index} className={`day-number ${index + 1 === currentDayOfMonth ? 'selected' : ''}`}>
+                  <span className="number">{index + 1}</span>
+                </div>
+              ))}
             </div>
           </div>
         </div>
