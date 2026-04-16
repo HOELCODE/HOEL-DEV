48093084dc17a0e9236d487ee87341528212d50d
HOELCODE <hcostantini1@gmail.com>
2026-04-16 05:49:46 +0200

    gestion mois et année ok

--- frontend/src/pages/Calendrier.jsx
+++ frontend/src/pages/Calendrier.jsx
@@ -1,26 +1,54 @@
-
+import { useState } from "react";
 const Calendrier = () => {
+  // Tableau des mois et jours
+  const monthArray = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre'];
+  const daysArray = ['LUN', 'MAR', 'MER', 'JEU', 'VEN', 'SAM', 'DIM'];
+
+  // Récupération de la date actuelle, puis du mois et de l'année
+  const date = new Date();
+  const currentMonth = date.toLocaleDateString('default', { month: 'long' });
+  const currentYear = date.getFullYear();
+
+  // State pour le mois, avec une fonction pour passer au mois suivant ou précédent
+  const [month, setMonth] = useState(currentMonth);
+
+  // State pour l'année 
+  const [year, setYear] = useState(currentYear);
+  
+  const nextMonth = () => {
+    const currentIndex = monthArray.indexOf(month);
+    const nextIndex = (currentIndex + 1) % monthArray.length;
+    nextIndex === 0 ? setYear(year + 1) : setYear(year);
+    setMonth(monthArray[nextIndex]);
+  }
+
+  const lastMonth = () => {
+    const reverseMonthArray = monthArray.reverse();
+    const currentIndex = monthArray.indexOf(month);
+    const lastIndex = (currentIndex + 1) % monthArray.length;
+    lastIndex === 0 ? setYear(year - 1) : setYear(year);
+    setMonth(reverseMonthArray[lastIndex]);
+  }
+
+
+
   return (
     <div className="Calendrier">
       <h1>Calendrier</h1>
       <div className="calendrier-app">
         <div className="calendrier-left">
           <div className="month-year">
-            <h2>May, 2024</h2>
+            <h2>{month}, {year}</h2>
             <div className="calendrier-btn">
-              <button className="btn-left"><i className="fa-solid fa-chevron-left"></i></button>
-              <button className="btn-right"><i className="fa-solid fa-chevron-right"></i></button>
+              <button className="btn-left" onClick={lastMonth}><i className="fa-solid fa-chevron-left"></i></button>
+              <button className="btn-right" onClick={nextMonth}><i className="fa-solid fa-chevron-right"></i></button>
             </div>
           </div>
           <div className="days">
             <div className="days-title">
-              <span className="day-title">MON</span>
-              <span className="day-title">TUE</span>
-              <span className="day-title">WED</span>
-              <span className="day-title">THU</span>
-              <span className="day-title">FRI</span>
-              <span className="day-title">SAT</span>
-              <span className="day-title">SUN</span>
+              {daysArray.map((day, index) => (
+                <span key={index} className="day-title">{day}</span>
+              ))}
             </div>
             <div className="day">
               <div className="day-number"><span className="number">1</span></div>
